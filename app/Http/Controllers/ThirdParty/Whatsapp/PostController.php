224345de12e860476bc51d17d44a6390d72b0380
madkuman <akhmad.kuncara@gmail.com>
2024-09-25 11:08:07 +0700

    menambah variabel lokasi di controller whatsapp

--- app/Http/Controllers/ThirdParty/Whatsapp/PostController.php
+++ app/Http/Controllers/ThirdParty/Whatsapp/PostController.php
@@ -13,7 +13,13 @@ class PostController extends Controller
         // $token = config('medify.third-party.whatsapp.token');
         $target = $request->input('target');
         $nama = $request->input('nama_pasien');
+        $lokasi = $request->input('lokasi');
         // dd($token, $target, $nama);
+        if ($lokasi == 12 || $lokasi == 13 || $lokasi == 11) {
+            $loket = 1;
+        } else {
+            $loket = 2;
+        }
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
@@ -27,7 +33,7 @@ class PostController extends Controller
             CURLOPT_CUSTOMREQUEST => 'POST',
             CURLOPT_POSTFIELDS => array(
                 'target' => $target,
-                'message' => 'Halo Bpk/Ibu ' . $nama . ', kami menginfokan bahwa obat resep Anda sudah selesai diproses, selanjutnya Bpk/Ibu dapat mengambilnya di loket penyerahan. Salam kami dari Instalasi Farmasi RS Jiwa Menur Surabaya',
+                'message' => 'Halo Bpk/Ibu ' . $nama . ', kami menginfokan bahwa obat resep Anda sudah selesai diproses, selanjutnya Bpk/Ibu dapat mengambilnya di loket penyerahan farmasi lantai ' . $loket . ' dengan membawa kartu identitas pasien maksimal 24 jam setelah pesan ini diterima. Salam kami dari Instalasi Farmasi RS Jiwa Menur Surabaya',
                 'countryCode' => '62', //optional
             ),
             CURLOPT_HTTPHEADER => array(
