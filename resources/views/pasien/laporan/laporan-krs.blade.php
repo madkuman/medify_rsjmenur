576b3c24e96df46600be142304ca82a731d23382
madkuman <akhmad.kuncara@gmail.com>
2024-11-12 11:18:19 +0700

    menambah kolom phone pasien dan phone kerabat

--- resources/views/pasien/laporan/laporan-krs.blade.php
+++ resources/views/pasien/laporan/laporan-krs.blade.php
@@ -1,16 +1,17 @@
 <table>
     <thead>
         <tr>
-            <th colspan="16" >LAPORAN PASIEN KRS</th>
+            <th colspan="16">LAPORAN PASIEN KRS</th>
         </tr>
         <tr>
-            <th colspan="16" style="text-transform: uppercase;">TEMPAT LAYANAN - {{$lokasi}}</th>
+            <th colspan="16" style="text-transform: uppercase;">TEMPAT LAYANAN - {{ $lokasi }}</th>
         </tr>
         <tr>
-            <th colspan="16" style="text-transform: uppercase;">{{$start->format('d-m-Y')}} s/d {{$end->format('d-m-Y')}}</th>
+            <th colspan="16" style="text-transform: uppercase;">{{ $start->format('d-m-Y') }} s/d
+                {{ $end->format('d-m-Y') }}</th>
         </tr>
         <tr>
-            <th colspan="16" style="text-transform: uppercase;">{{config('app.name')}}</th>
+            <th colspan="16" style="text-transform: uppercase;">{{ config('app.name') }}</th>
         </tr>
         <tr>
             <th></th>
@@ -20,6 +21,9 @@
             <th>Nama Pasien</th>
             <th>No. RM</th>
             <th>Alamat</th>
+            <th>No HP Pasien</th>
+            <th>Nama Keluarga</th>
+            <th>No HP Keluarga</th>
             <th>Jenis Bayar</th>
             <th>Kelas Bayar</th>
             <th>No. Asuransi</th>
@@ -35,45 +39,48 @@
         </tr>
     </thead>
     <tbody>
-        @if(!empty($laporan))
-        @foreach($laporan as $idx => $pasien)
-        <tr>
-            <td>{{$idx+1}}</td>
-            <td>{{$pasien['nama']}}</td>
-            <td>{{$pasien['no_rm']}}</td>
-            <td>{{$pasien['alamat']}}</td>
-            <td>{{$pasien['jenis_bayar']}}</td>
-            <td>{{$pasien['kelas_bayar']}}</td>
-            <td>{{$pasien['no_asuransi']}}</td>
-            <td>{{$pasien['no_sep']}}</td>
-            <td>{{$pasien['lokasi']}}</td>
-            <td>{{$pasien['dpjp']}}</td>
-            <td>
-                @if(!empty($pasien['diagnosis']))
-                @foreach($pasien['diagnosis'] as $diagnosis)
-                - {{$diagnosis}}
-                <br style="mso-data-placement:same-cell;" />
-                @endforeach
-                @else
-                -
-                @endif
-            </td>
-            <td>
-                @if(!empty($pasien['tindakan']))
-                @foreach($pasien['tindakan'] as $tindakan)
-                - {{$tindakan}}
-                <br style="mso-data-placement:same-cell;" />
-                @endforeach
-                @else
-                -
-                @endif
-            </td>
-            <td>{{$pasien['mrs_at']}}</td>
-            <td>{{$pasien['krs_at']}}</td>
-            <td>{{$pasien['krs_status']}}</td>
-            <td>{{$pasien['krs_alasan']}}</td>
-        </tr>
-        @endforeach
+        @if (!empty($laporan))
+            @foreach ($laporan as $idx => $pasien)
+                <tr>
+                    <td>{{ $idx + 1 }}</td>
+                    <td>{{ $pasien['nama'] }}</td>
+                    <td>{{ $pasien['no_rm'] }}</td>
+                    <td>{{ $pasien['alamat'] }}</td>
+                    <td>{{ $pasien['phone'] }}</td>
+                    <td>{{ $pasien['wali_name'] }}</td>
+                    <td>{{ $pasien['wali_phone'] }}</td>
+                    <td>{{ $pasien['jenis_bayar'] }}</td>
+                    <td>{{ $pasien['kelas_bayar'] }}</td>
+                    <td>{{ $pasien['no_asuransi'] }}</td>
+                    <td>{{ $pasien['no_sep'] }}</td>
+                    <td>{{ $pasien['lokasi'] }}</td>
+                    <td>{{ $pasien['dpjp'] }}</td>
+                    <td>
+                        @if (!empty($pasien['diagnosis']))
+                            @foreach ($pasien['diagnosis'] as $diagnosis)
+                                - {{ $diagnosis }}
+                                <br style="mso-data-placement:same-cell;" />
+                            @endforeach
+                        @else
+                            -
+                        @endif
+                    </td>
+                    <td>
+                        @if (!empty($pasien['tindakan']))
+                            @foreach ($pasien['tindakan'] as $tindakan)
+                                - {{ $tindakan }}
+                                <br style="mso-data-placement:same-cell;" />
+                            @endforeach
+                        @else
+                            -
+                        @endif
+                    </td>
+                    <td>{{ $pasien['mrs_at'] }}</td>
+                    <td>{{ $pasien['krs_at'] }}</td>
+                    <td>{{ $pasien['krs_status'] }}</td>
+                    <td>{{ $pasien['krs_alasan'] }}</td>
+                </tr>
+            @endforeach
         @endif
     </tbody>
 </table>
