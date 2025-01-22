fdd880392a818aacefe1bf83afec444ee2334e92
Firmansyahdwin <firmansyahdwi21@gmail.com>
2025-01-22 10:06:33 +0700

    Membuat Form baru (Surat Keterangan Pemeriksaaan EKG)

--- resources/views/kasus/alatbantu/index/form-lainnya.blade.php
+++ resources/views/kasus/alatbantu/index/form-lainnya.blade.php
@@ -1,4 +1,3 @@
-
 <div class="col-12">
     <h4>Form Lainnya</h4>
 </div>
@@ -24,7 +23,7 @@
         </li> --}}
 
         <li class="col-md-4 text-center" style="display: none;">
-            <a class="block block-link-pop block-themed" href="{{url()->current()}}/sk-dirawat" target="_blank">
+            <a class="block block-link-pop block-themed" href="{{ url()->current() }}/sk-dirawat" target="_blank">
                 <div class="block-header bg-primary">
                     <h3 class="block-title">Surat Keterangan Dirawat</h3>
                 </div>
@@ -55,7 +54,7 @@
 
         <li class="col-md-4 text-center">
 
-            <a class="block block-link-pop block-themed" href="{{url()->current()}}/surat-keterangan">
+            <a class="block block-link-pop block-themed" href="{{ url()->current() }}/surat-keterangan">
                 <div class="block-header bg-primary">
                     <h3 class="block-title">Surat Keterangan Istirahat / Dirawat / Sakit</h3>
                 </div>
@@ -85,7 +84,7 @@
         </li> --}}
 
         <li class="col-md-4 text-center">
-            <a class="block block-link-pop block-themed" href="{{url()->current()}}/surat-pasien-pulang-rumah-sakit">
+            <a class="block block-link-pop block-themed" href="{{ url()->current() }}/surat-pasien-pulang-rumah-sakit">
                 <div class="block-header bg-primary">
                     <h3 class="block-title">Surat Pasien Pulang / Keluar Rumah Sakit</h3>
                 </div>
@@ -100,7 +99,8 @@
         </li>
 
         <li class="col-md-4 text-center">
-            <a class="block block-link-pop block-themed" href="{{url('kasus')}}/{{$kasus->nomor_kasus}}/asesmen/surat-nasehat-pulang">
+            <a class="block block-link-pop block-themed"
+                href="{{ url('kasus') }}/{{ $kasus->nomor_kasus }}/asesmen/surat-nasehat-pulang">
                 <div class="block-header bg-primary">
                     <h3 class="block-title">Surat Nasehat Pulang</h3>
                 </div>
@@ -115,7 +115,7 @@
         </li>
 
         <li class="col-md-4 text-center">
-            <a class="block block-link-pop block-themed" href="{{url()->current()}}/surat-keterangan-dalam-perawatan">
+            <a class="block block-link-pop block-themed" href="{{ url()->current() }}/surat-keterangan-dalam-perawatan">
                 <div class="block-header bg-primary">
                     <h3 class="block-title">Surat Keterangan Dalam Perawatan</h3>
                 </div>
@@ -130,7 +130,8 @@
         </li>
 
         <li class="col-md-4 text-center">
-            <a class="block block-link-pop block-themed" href="{{url('kasus')}}/{{$kasus->nomor_kasus}}/asesmen/surat-pernyataan-kesanggupan-pembiayaan">
+            <a class="block block-link-pop block-themed"
+                href="{{ url('kasus') }}/{{ $kasus->nomor_kasus }}/asesmen/surat-pernyataan-kesanggupan-pembiayaan">
                 <div class="block-header bg-primary">
                     <h3 class="block-title">Surat Pernyataan Kesanggupan Pembiayaan</h3>
                 </div>
@@ -140,6 +141,81 @@
                     </p>
                     <h5 class="mb-5 title">Surat Pernyataan Kesanggupan Pembiayaan</h5>
                     <p class="desc">Surat Pernyataan Kesanggupan Pembiayaan</p>
+                </div>
+            </a>
+        </li>
+
+        <li class="col-md-4 text-center">
+            <a class="block block-link-pop block-themed" href="{{ url()->current() }}/surat-keterangan-fisik">
+                <div class="block-header bg-primary">
+                    <h3 class="block-title">Surat Keterangan Sehat Fisik</h3>
+                </div>
+                <div class="block-content">
+                    <p class="mt-5 mb-10">
+                        <i class="fa fa-calculator fa-4x "></i>
+                    </p>
+                    <h5 class="mb-5 title">Surat Keterangan Sehat Fisik</h5>
+                    <p class="desc">Surat Keterangan Sehat Fisik</p>
+                </div>
+            </a>
+        </li>
+
+        <li class="col-md-4 text-center">
+            <a class="block block-link-pop block-themed" href="{{ url()->current() }}/surat-keterangan-jiwa">
+                <div class="block-header bg-primary">
+                    <h3 class="block-title">Surat Keterangan Sehat Jiwa</h3>
+                </div>
+                <div class="block-content">
+                    <p class="mt-5 mb-10">
+                        <i class="fa fa-calculator fa-4x "></i>
+                    </p>
+                    <h5 class="mb-5 title">Surat Keterangan Sehat Jiwa</h5>
+                    <p class="desc">Surat Keterangan Sehat Jiwa</p>
+                </div>
+            </a>
+        </li>
+
+        <li class="col-md-4 text-center">
+            <a class="block block-link-pop block-themed" href="{{ url()->current() }}/surat-keterangan-napza">
+                <div class="block-header bg-primary">
+                    <h3 class="block-title">Surat Keterangan Pemeriksaan Napza</h3>
+                </div>
+                <div class="block-content">
+                    <p class="mt-5 mb-10">
+                        <i class="fa fa-calculator fa-4x "></i>
+                    </p>
+                    <h5 class="mb-5 title">Surat Keterangan Pemeriksaan Napza</h5>
+                    <p class="desc">Surat Keterangan Pemeriksaan Napza</p>
+                </div>
+            </a>
+        </li>
+
+        <li class="col-md-4 text-center">
+            <a class="block block-link-pop block-themed" href="{{ url()->current() }}/surat-keterangan-hiv">
+                <div class="block-header bg-primary">
+                    <h3 class="block-title">Surat Keterangan Bebas HIV</h3>
+                </div>
+                <div class="block-content">
+                    <p class="mt-5 mb-10">
+                        <i class="fa fa-calculator fa-4x "></i>
+                    </p>
+                    <h5 class="mb-5 title">Surat Keterangan Bebas HIV</h5>
+                    <p class="desc">Surat Keterangan Bebas HIV</p>
+                </div>
+            </a>
+        </li>
+
+        <li class="col-md-4 text-center">
+            <a class="block block-link-pop block-themed" href="{{ url()->current() }}/surat-keterangan-pemeriksaan-ekg">
+                <div class="block-header bg-primary">
+                    <h3 class="block-title">Surat Keterangan Pemeriksaan EKG</h3>
+                </div>
+                <div class="block-content">
+                    <p class="mt-5 mb-10">
+                        <i class="fa fa-calculator fa-4x "></i>
+                    </p>
+                    <h5 class="mb-5 title">Surat Keterangan Pemeriksaan EKG</h5>
+                    <p class="desc">Surat Keterangan Pemeriksaan EKG</p>
                 </div>
             </a>
         </li>
---
+++ resources/views/kasus/alatbantu/surat-keterangan-napza/form.blade.php
@@ -0,0 +1,199 @@
+<input type="hidden" name="alatbantu_id" value="">
+<input type="hidden" name="kasus_id" value="{{ $kasus->id }}">
+
+<table style="width:100%">
+    <tr>
+        <td class="position-relative" colspan="1" rowspan="1">
+            <h5 style="text-align: center; padding: 20px 0px">SURAT KETERANGAN PEMERIKSAAN NAPZA</h5>
+        </td>
+    </tr>
+</table>
+<table style="width:100%">
+    <tr>
+        <td width="20%" class="position-relative" colspan="1" rowspan="1">
+            <span>Nomor</span>
+        </td>
+        <td width="2%" class="position-relative" colspan="1" rowspan="1"><span>:</span></td>
+        <td width="6%">400.7 /</td>
+        <td width="10%"><input type="number" class="form-control" name="nomor" value=""></td>
+        <td> / 2 / 102.8 / {{ date('Y') }}</td>
+    </tr>
+</table>
+<table style="width: 100%">
+    <tr>
+        <td>
+            <h6>Yang bertanda tangan di bawah ini :</h6>
+        </td>
+    </tr>
+</table>
+<table style="width: 100%">
+    <tr>
+        <td width="20%" class="position-relative" colspan="1" rowspan="1">
+            <span>Dokter Pemeriksa</span>
+        </td>
+        <td width="2%" class="position-relative" colspan="1" rowspan="1"><span>:</span></td>
+        <td class="position-relative" colspan="1" rowspan="1">
+            <div class="form-group medify-form-genv4-input-container">
+                <select class="form-control js-select2" style="width: 80%" name="dpjp">
+                    <option value="">-</option>
+                    @foreach ($dokter as $item)
+                        <option value="{{ $item->id }}">
+                            {{ $item->name }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="form-group medify-form-genv4-view-container">
+
+            </div>
+        </td>
+    </tr>
+</table>
+<table style="width: 100%">
+    <tr>
+        <td class="position-relative" colspan="1" rowspan="1">
+            <h6>Menerangkan dengan sebenarnya bahwa :</h6>
+        </td>
+    </tr>
+</table>
+<table style="width: 100%">
+    <tr>
+        <td width="20%" class="position-relative" colspan="1" rowspan="1"><span>Nama</span></td>
+        <td width="2%" class="position-relative" colspan="1" rowspan="1">:</td>
+        <td class="position-relative" colspan="1" rowspan="1">
+            {{ ucwords(strtolower($kasus->pasien->name)) }}
+        </td>
+    </tr>
+    <tr>
+        <td width="20%" class="position-relative" colspan="1" rowspan="1"><span>Jenis Kelamin</span></td>
+        <td width="2%" class="position-relative" colspan="1" rowspan="1"><span>:</span></td>
+        <td class="position-relative" colspan="1" rowspan="1">
+            {{ $kasus->pasien->jenis_kelamin }}
+        </td>
+    </tr>
+    <tr>
+        <td width="20%" class="position-relative" colspan="1" rowspan="1"><span>Tanggal Lahir</span></td>
+        <td width="2%" class="position-relative" colspan="1" rowspan="1"><span>:</span></td>
+        <td class="position-relative" colspan="1" rowspan="1">
+            {{ indonesian_date($kasus->pasien->date_of_birth) }}
+        </td>
+    </tr>
+    <tr>
+        <td width="20%" class="position-relative" colspan="1" rowspan="1"><span>Alamat</span></td>
+        <td width="2%" class="position-relative" colspan="1" rowspan="1"><span>:</span></td>
+        <td class="position-relative" colspan="1" rowspan="1">
+            {{ ucwords(strtolower($kasus->pasien->address)) }}, Kel.
+            {{ ucwords(strtolower($kasus->pasien->alamat_kelurahan->nama)) }}, Kec.
+            {{ ucwords(strtolower($kasus->pasien->alamat_kecamatan->nama)) }},
+            {{ ucwords(strtolower($kasus->pasien->alamat_kota->nama)) }}
+        </td>
+    </tr>
+    <tr>
+        <td width="20%" class="position-relative" colspan="1" rowspan="1"><span>Pendidikan</span></td>
+        <td width="2%" class="position-relative" colspan="1" rowspan="1"><span>:</span></td>
+        <td class="position-relative" colspan="1" rowspan="1">
+            <div class="form-group medify-form-genv4-input-container">
+                <input type="text" class="form-control" name="pendidikan" value="">
+            </div>
+            <div class="form-group medify-form-genv4-view-container">
+            </div>
+        </td>
+    </tr>
+</table>
+<table style="width:100%">
+    <tr>
+        <td class="position-relative" colspan="1" rowspan="1">
+            <h6>Pada pemeriksaan tanggal :</h6>
+        </td>
+    </tr>
+</table>
+<table style="width: 100%">
+    <tr>
+        <td width="20%" class="position-relative" colspan="1" rowspan="1"><span>Tanggal</span></td>
+        <td width="2%" class="position-relative" colspan="1" rowspan="1"><span>:</span></td>
+        <td class="position-relative" colspan="1" rowspan="1">
+            <div class="form-group medify-form-genv4-input-container">
+                <input type="date" class="form-control" name="tanggal_pemeriksaan" value="">
+            </div>
+            <div class="form-group medify-form-genv4-view-container">
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <td width="20%" class="position-relative" colspan="1" rowspan="1"><span>Jam</span></td>
+        <td width="2%" class="position-relative" colspan="1" rowspan="1"><span>:</span></td>
+        <td class="position-relative" colspan="1" rowspan="1">
+            <div class="form-group medify-form-genv4-input-container">
+                <input type="time" class="form-control" name="jam" value="">
+            </div>
+            <div class="form-group medify-form-genv4-view-container">
+            </div>
+        </td>
+    </tr>
+</table>
+<table style="width: 100%">
+    <tr>
+        <td width="20%" class="position-relative" colspan="1" rowspan="1"><span>Parameter</span></td>
+        <td width="2%" class="position-relative" colspan="1" rowspan="1"><span>:</span></td>
+        <td class="position-relative" colspan="1" rowspan="1">
+            <div class="form-check medify-form-genv4-input-container">
+                <input class="form-check-input" type="checkbox" value="metamphethamine" id="metamphethamine"
+                    name="metamphethamine">
+                <label class="form-check-label" for="metamphethamine">
+                    Metamphethamine
+                </label>
+            </div>
+            <div class="form-check medify-form-genv4-input-container">
+                <input class="form-check-input" type="checkbox" value="amphetamine" id="amphetamine"
+                    name="amphetamine">
+                <label class="form-check-label" for="amphetamine">
+                    Amphetamine
+                </label>
+            </div>
+            <div class="form-check medify-form-genv4-input-container">
+                <input class="form-check-input" type="checkbox" value="morphine_heroin" id="morphine_heroin"
+                    name="morphine_heroin">
+                <label class="form-check-label" for="morphine_heroin">
+                    Morphine / Heroin
+                </label>
+            </div>
+            <div class="form-check medify-form-genv4-input-container">
+                <input class="form-check-input" type="checkbox" value="mariyuana_thc" id="mariyuana_thc"
+                    name="mariyuana_thc">
+                <label class="form-check-label" for="mariyuana_thc">
+                    Mariyuana / THC
+                </label>
+            </div>
+            <div class="form-check medify-form-genv4-input-container">
+                <input class="form-check-input" type="checkbox" value="benzodiazepine" id="benzodiazepine"
+                    name="benzodiazepine">
+                <label class="form-check-label" for="benzodiazepine">
+                    Benzodiazepine
+                </label>
+            </div>
+            <div class="form-check medify-form-genv4-input-container">
+                <input class="form-check-input" type="checkbox" value="coccain" id="coccain" name="coccain">
+                <label class="form-check-label" for="coccain">
+                    Coccain
+                </label>
+            </div>
+            <div class="form-group medify-form-genv4-view-container">
+            </div>
+        </td>
+    </tr>
+</table>
+<table style="width: 100%">
+    <tr>
+        <td width="20%" class="position-relative" colspan="1" rowspan="1"><span>Surat keterangan ini dibuat
+                sebagai </span></td>
+        <td width="2%" class="position-relative" colspan="1" rowspan="1"><span>:</span></td>
+        <td class="position-relative" colspan="1" rowspan="1">
+            <div class="form-group medify-form-genv4-input-container">
+                <textarea class="form-control" name="syarat"></textarea>
+            </div>
+            <div class="form-group medify-form-genv4-view-container">
+            </div>
+        </td>
+    </tr>
+</table>
+{{-- @TODO: create validation --}}
