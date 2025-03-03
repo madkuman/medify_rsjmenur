5a12d54a7d922268ba9c752132b211ba98601b00
madkuman <akhmad.kuncara@gmail.com>
2025-03-03 07:44:15 +0700

    perbaikan form gelang bayi

--- resources/views/pasien/profile/print-gelang-bayi.blade.php
+++ resources/views/pasien/profile/print-gelang-bayi.blade.php
@@ -1,47 +1,69 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Print Gelang  - {{$pasien->name}}</title>
-	<style type="text/css">
-	table {
-		border-collapse: collapse;
-		font-size: 11px;
-	}
-	body{
-		margin: 0px;
-		margin-top: 0px;
-	}
-	@page{
-		margin: 0px;
-	}
-	.centered{
-		text-align: center;
-	}
-	.dummy{
-		color: white;
-	}
-	td{
-		margin: 0px;
-		padding: 0px;
-	}
-</style>
+    <title>Print Gelang - {{ $pasien->name }}</title>
+    <style type="text/css">
+        table {
+            border-collapse: collapse;
+            font-size: 10px;
+        }
+
+        body {
+            margin: 0px;
+            margin-top: 20px;
+            margin-left: 625px;
+        }
+
+        @page {
+            size: 215mm 30mm portrait;
+            margin: 0px;
+        }
+
+        .centered {
+            text-align: left;
+        }
+
+        .dummy {
+            color: white;
+        }
+
+        td {
+            margin: 0px;
+            padding: 0px;
+        }
+
+        .norm {
+            font-size: 12px;
+        }
+    </style>
 </head>
+
 <body>
-<table style="width: 100vw">
-	<tr>
-		<td rowspan="3" style="width:45% "></td>
-		<td rowspan="3" style="width:7% ">{!!$barcode!!}</td>
-		<td class="centered" style="width: 18%">#{{$pasien->no_rm_formatted}}</td>
-		<td class="dummy" style="width: 30%">.</td>
-	</tr>
-	<tr>
-		<td class="centered"><b>{{$pasien->name}}</b></td>
-		<td></td>
-	</tr>
-	<tr>
-		<td class="centered">{{date("d-m-Y", strtotime($pasien->date_of_birth))}} / {{$pasien->age}} Thn</td>
-		<td></td>
-	</tr>
-</table>
+    <table style="width: 100vw"">
+        {{-- <tr>
+            <td rowspan="3" style="width:45% "></td>
+            <td rowspan="3" style="width:7% ">{!! $barcode !!}</td>
+            <td class="centered" style="width: 18%">#{{ $pasien->no_rm_formatted }}</td>
+            <td class="dummy" style="width: 30%">.</td>
+        </tr> --}}
+        {{-- <tr>
+            <td class="dummy" style="width: 40%">.</td>
+        </tr> --}}
+        <tr class="norm">
+            <td style="width:30% " class="centered">#{{ $pasien->no_rm_formatted }}</td>
+        </tr>
+        <tr>
+            <td style="width:60%" class="centered"><b>{{ $pasien->name }}</b></td>
+            <td></td>
+        </tr>
+        <tr>
+            <td style="width:70% " class="centered">{{ date('d-m-Y', strtotime($pasien->date_of_birth)) }} /
+                {{ $pasien->age }} Thn /
+                {{ $pasien->jk->nama }}</td>
+            <td></td>
+        </tr>
+    </table>
 </body>
+
 </html>
